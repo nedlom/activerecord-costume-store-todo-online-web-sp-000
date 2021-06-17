42b42539c818bbca6d560f19a8e0b774e6a4a43d
Nick Lombardi <nelom3@gmail.com>
2021-06-17 15:22:52 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,9 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |t|
+      t.
+    end
+  end
+end
