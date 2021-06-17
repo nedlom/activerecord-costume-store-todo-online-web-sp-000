b7b7ebf10c0042935104c8bb262d578b59faabdb
Nick Lombardi <nelom3@gmail.com>
2021-06-17 15:29:25 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,9 @@
 # Create your haunted_houses migration 
 
 class CreateHauntedHouse < ActiveRecord::Migration[4.2]
+  class change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.
