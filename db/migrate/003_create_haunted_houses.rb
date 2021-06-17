28d04571d5d61ae29a274ba6306c960e8c161eb9
Nick Lombardi <nelom3@gmail.com>
2021-06-17 15:30:43 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,4 +8,6 @@ class CreateHauntedHouse < ActiveRecord::Migration[4.2]
       t.string :theme
       t.float :price
       t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
       
