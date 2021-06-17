05b5e8ff5c3eeeb54d091d90fa9a022a60651c8d
Nick Lombardi <nelom3@gmail.com>
2021-06-17 14:51:13 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,7 +12,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      :price
+      t.float :price
       :size
       :image_url
     end
