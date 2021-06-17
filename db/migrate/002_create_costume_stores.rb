63e122c4ea1f98d7a2c2bf05fc3b4b480d9d89f4
Nick Lombardi <nelom3@gmail.com>
2021-06-17 15:24:29 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      
+      t.integer :num_of_employees
+      t.boolean :
     end
   end
 end
